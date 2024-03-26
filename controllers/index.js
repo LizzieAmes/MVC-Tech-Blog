bf4d6d3070ff20603685cdddd638257b2e0c6a8d
Lizzie <lizziejacoboo@gmail.com>
2024-03-25 19:14:33 -0600

    added login/logout/signup function to index.js, handlebars for dashboard, login and signup

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,105 @@
-const router = require('express').Router();
+const express = require('express');
+const bcrypt = require('bcrypt');
+const { User } = require('../models');
+const router = express.Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
+const withAuth = require('../utils/auth');
+
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/dashboard');
+    return;
+  }
+  res.render('login');
+});
+
+router.post('/login', async (req, res) => {
+  try {
+    const userData = await User.findOne({
+      where: { username: req.body.username },
+    });
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect username or password, please try again' });
+      return;
+    }
+
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      userData.password
+    );
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect username or password, please try again' });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.userId = userData.id;
+      req.session.username = userData.username;
+      req.session.loggedIn = true;
+
+      res.json({ user: userData, message: 'You are now logged in!' });
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get('/signup', (req, res) => {
+  res.render('signup');
+});
+
+router.post('/signup', async (req, res) => {
+  try {
+    const newUser = await User.create({
+      username: req.body.username,
+      password: req.body.password, 
+    });
+
+    req.session.save(() => {
+      req.session.userId = newUser.id;
+      req.session.username = newUser.username;
+      req.session.loggedIn = true;
+
+      res.json(newUser);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// Handle logout
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+router.get('/dashboard', withAuth, async (req, res) => {
+  try {
+    const userPosts = await Post.findAll({
+      where: {
+        userId: req.session.userId,
+      },
+      include: [{ model: User }],
+    });
+    const posts = userPosts.map((post) => post.get({ plain: true }));
+    res.render('dashboard', { posts, loggedIn: req.session.loggedIn });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
